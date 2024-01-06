9ac4c5f4764f25336ead6eabcd904f7e9db8bf1d
MathevsBatista <matheusisidoro09@gmail.com>
2024-01-06 14:15:51 -0300

    Adds style and change dom manipulation

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,53 @@
 buttonRock = document.querySelector(".rock");
 buttonPaper = document.querySelector(".paper");
 buttonScissor = document.querySelector(".scissor");
-pResults = document.querySelector(".results");
+textComputerChoice = document.querySelector(".computerChoice .wins h4");
+imgComputerChoice = document.querySelector(".computerChoice .choiceImg img");
+changeUserChoice = document.querySelector(".userChoice .wins h4");
+imgUserChoice = document.querySelector(".userChoice .choiceImg img");
+h3Results = document.querySelector(".scoreBoard .result");
+ScoreBoard= document.querySelector(".scoreBoard .score");
+roundResult = document.querySelector(".scoreBoard h2");
+see = document.querySelectorAll(".see");
+
 let userWin = 0, computerWin = 0;
 
 buttonRock.addEventListener("click", () => {
-    pResults.textContent = playRound("pedra", getComputerChoice());
+    let computerChoice = getComputerChoice();
+    let userChoice = "pedra";
+    const results = playRound(userChoice, computerChoice);
+    constructResult(userChoice, computerChoice, results);
 });
 
 buttonPaper.addEventListener("click", () => {
-    pResults.textContent = playRound("papel", getComputerChoice());
+    let computerChoice = getComputerChoice();
+    let userChoice = "papel";
+    const results = playRound(userChoice, computerChoice);
+    constructResult(userChoice, computerChoice, results);
 });
 
 buttonScissor.addEventListener("click", () => {
-    pResults.textContent = playRound("tesoura", getComputerChoice());
+    let computerChoice = getComputerChoice();
+    let userChoice = "tesoura";
+    const results = playRound(userChoice, computerChoice);
+    constructResult(userChoice, computerChoice, results);
 });
+
+function constructResult(userChoice, computerChoice, results) {
+    textComputerChoice.textContent = computerChoice;
+    imgComputerChoice.setAttribute('src', "images/" + computerChoice + ".svg");
+
+    changeUserChoice.textContent = userChoice;
+    imgUserChoice.setAttribute('src', "images/" + userChoice + ".svg");
+
+    roundResult.textContent = results[0]; // Win, loss or draw
+    h3Results.textContent = results[1]; // Round result
+    ScoreBoard.textContent = results[2]; // Current score
+    
+    see.forEach((seeLocal) => {
+	seeLocal.setAttribute('style', "display: flex");
+    });
+}
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -29,21 +62,21 @@ function getComputerChoice() {
 
 function playRound(userChoice, computerChoice) {
     if(userChoice == computerChoice) {
-	return `Empate! ${userChoice} empata com ${computerChoice}\n${getScoreboard(userWin, computerWin)}`;
+	return [`Empate!`, `${userChoice} empata com ${computerChoice}`, `${getScoreboard(userWin, computerWin)}`];
     } else if(
 	(userChoice == "pedra" && computerChoice == "papel") ||
 	(userChoice == "papel" && computerChoice == "tesoura") ||
 	(userChoice == "tesoura" && computerChoice == "pedra")
     ) {
 	computerWin++;
-	return `Você perdeu! ${userChoice} perde para ${computerChoice}\n${getScoreboard(userWin, computerWin)}`;
+	return [`Você perdeu a rodada!`, `${userChoice} perde para ${computerChoice}`, `${getScoreboard(userWin, computerWin)}`];
     } else if(
 	(userChoice == "pedra" && computerChoice == "tesoura") ||
 	(userChoice == "papel" && computerChoice == "pedra") ||
 	(userChoice == "tesoura" && computerChoice == "papel")
     ) {
 	userWin++;
-	return `Você ganhou! ${userChoice} vence ${computerChoice}\n${getScoreboard(userWin, computerWin)}`;
+	return [`Você ganhou a rodada!`, `${userChoice} vence ${computerChoice}`, `${getScoreboard(userWin, computerWin)}`];
     }
 }
 
